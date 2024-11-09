c32126fe198202cb105c87a907d0ff6e6c5e0ffb
Rasya <school.rasya@gmail.com>
2024-11-09 19:45:28 +0700

    Update controller.cpp

--- src/FP_Magang/src/controller.cpp
+++ src/FP_Magang/src/controller.cpp
@@ -12,17 +12,20 @@
 #include <unistd.h>
 
 using namespace std;
+using namespace ros;
 
 class Controller
 {
 private:
-    ros::NodeHandle nh;
-    ros::Publisher pc2bs_pub;
-    ros::Subscriber bs2pc_sub;
-    ros::Subscriber pc2pc_sub;
-    ros::Publisher toImagePicker_pub;
+
+    NodeHandle nh;
+
+    Publisher pubPC2BS;
+    Subscriber subBS2PC;
+    Subscriber subPC2PC;
 
     float interval = 0;
+
     float degToRad(float deg)
     {
         return deg * M_PI / 180.0;
@@ -37,6 +40,31 @@ private:
     float sigmoid(float interval)
     {
         return 1.0 / (1.0 + exp(-2.5 * (2 * interval - 1)));
+    }
+
+    float normalizeAngle(float angle) {
+        
+        angle = fmod(angle, 360.0);
+        // if (angle < 0) {
+        //     angle += 360.0;
+        // }
+        return angle;
+    }
+
+    float angleBetweenVectors(float ax, float ay, float bx, float by) {
+        
+        float x = bx - ax;
+        float y = by - ay;
+
+        if (x >= 0 && y >= 0) {
+            return 225.0f;
+        } else if (x < 0 && y >= 0) {
+            return 135.0f;
+        } else if (x >= 0 && y < 0) {
+            return 315.0f;
+        } else if (x < 0 && y < 0) {
+            return 45.0f;
+        }
     }
 
     char getKey()
@@ -69,37 +97,48 @@ private:
 
         if (msg.status == 3)
         {
+            if (target[0] != msg.tujuan_x || target[1] != msg.tujuan_y) {
+                blankFrame = 0;
+                currStep = 1;
+                ROS_INFO("Y %.2f %.2f", target[0], msg.tujuan_x);
+
+                target[0] = msg.tujuan_x;
+                target[1] = msg.tujuan_y;
+            }
+
             target[0] = msg.tujuan_x;
             target[1] = msg.tujuan_y;
         }
 
-        if (bsMsg.status != msg.status && (msg.status == 2 || msg.status == 4))
+        if (prevMsg.status != msg.status && (msg.status == 2 || msg.status == 4))
         {
             blankFrame = 0;
             currStep = 0;
         }
 
-        bsMsg = msg;
+        prevMsg = msg;
+        prevMsg.th = normalizeAngle(prevMsg.th);
+
         updateRobotPos(msg.enc_left, msg.enc_right);
     }
 
     void pc2pcCallback(const FP_Magang::PC2PC &msg)
     {
-        if (bsMsg.status == 4 || bsMsg.status == 2) {
+        if (prevMsg.status == 4 || prevMsg.status == 2) {
             ballPos[0] = msg.follow_x;
             ballPos[1] = msg.follow_y;
         }
         // ballPos[0] = msg.follow_x;
         // ballPos[1] = msg.follow_y;
 
-        if (bsMsg.status == 2)
+        if (prevMsg.status == 2)
         {
             target[0] = ballPos[0];
             target[1] = ballPos[1];
 
             currStep = 1;
         }
-        else if (bsMsg.status == 4)
+        else if (prevMsg.status == 4)
         {
             float x = ballPos[0] - robotPos[0];
             float y = ballPos[1] - robotPos[1];
@@ -112,7 +151,7 @@ private:
     }
 
 public:
-    FP_Magang::BS2PC bsMsg;
+    FP_Magang::BS2PC prevMsg;
     FP_Magang::PC2BS msg;
 
     float robotPos[2] = {0, 0};
@@ -126,35 +165,35 @@ public:
 
     Controller()
     {
-        pc2bs_pub = nh.advertise<FP_Magang::PC2BS>("/pc2bs", 50);
+        pubPC2BS = nh.advertise<FP_Magang::PC2BS>("/pc2bs", 50);
 
-        bs2pc_sub = nh.subscribe("/bs2pc", 10, &Controller::bs2pcCallback, this);
-        pc2pc_sub = nh.subscribe("/pc2pc", 10, &Controller::pc2pcCallback, this);
+        subBS2PC = nh.subscribe("/bs2pc", 1, &Controller::bs2pcCallback, this);
+        subPC2PC = nh.subscribe("/pc2pc", 1, &Controller::pc2pcCallback, this);
     }
 
     void shootBall(float power)
     {
-        float ct = bsMsg.th;
+        float ct = prevMsg.th;
         float x = ballPos[0] + (power * sin(degToRad(ct)));
         float y = ballPos[1] + (power * cos(degToRad(ct)));
 
-        if (x + 27.7430 < 0)
-        {
-            x = -27.7430;
-        }
-        else if (x - 926.866219 > 0)
-        {
-            x = 926.866219;
-        }
+        // if (x + 27.7430 < 0)
+        // {
+        //     x = -27.7430;
+        // }
+        // else if (x - 926.866219 > 0)
+        // {
+        //     x = 926.866219;
+        // }
 
-        if (y + 31.4311 < 0)
-        {
-            y = -31.4311;
-        }
-        else if (y - 627.356796 > 0)
-        {
-            y = 627.356796;
-        }
+        // if (y + 31.4311 < 0)
+        // {
+        //     y = -31.4311;
+        // }
+        // else if (y - 627.356796 > 0)
+        // {
+        //     y = 627.356796;
+        // }
 
         ballPos[0] = x;
         ballPos[1] = y;
@@ -162,17 +201,18 @@ public:
         FP_Magang::PC2BS msg;
         msg.bola_x = x;
         msg.bola_y = y;
-        pc2bs_pub.publish(msg);
-        ros::spinOnce();
+        pubPC2BS.publish(msg);
+        spinOnce();
     }
 
     void updateMotor(float x, float y, float theta, bool manual = false)
     {
+
         float motorSpeed = 1000.0f;
 
         if (manual)
         {
-            float ct = -bsMsg.th;
+            float ct = -prevMsg.th;
             float sinus = sin(degToRad(ct));
             float cosinus = cos(degToRad(ct));
 
@@ -208,10 +248,10 @@ public:
         if ((round(x) == 0) && (round(y) == 0) && (round(theta / 10) == 0))
         {
             interval = 0;
-            if (bsMsg.status == 4)
-            {
-                currStep++;
-            }
+            // if (prevMsg.status == 4)
+            // {
+            //     currStep++;
+            // }
             return;
         }
 
@@ -222,8 +262,8 @@ public:
 
         msg.bola_x = ballPos[0];
         msg.bola_y = ballPos[1];
-        pc2bs_pub.publish(msg);
-        ros::spinOnce();
+        pubPC2BS.publish(msg);
+        spinOnce();
     }
 
     void function_1()
@@ -232,7 +272,8 @@ public:
 
         char key = getKey();
         float distToBall = 0;
-        float MOVE_SPEED = 100;
+
+        float MOVE_SPEED = 300;
 
         switch (key) {
         case 'w':
@@ -244,9 +285,9 @@ public:
         case 'd':
             updateMotor(-1*MOVE_SPEED, 0, 0, true); break;
         case 'q':
-            updateMotor(0, 0, 1*MOVE_SPEED, true); break;
+            updateMotor(0, 0, 10.0f*MOVE_SPEED, true); break;
         case 'e':
-            updateMotor(0, 0, -1*MOVE_SPEED, true); break;
+            updateMotor(0, 0, -10.0f*MOVE_SPEED, true); break;
         case 'z':
             distToBall = sqrt(pow((ballPos[0] - robotPos[0]), 2) + pow((ballPos[1] - robotPos[1]), 2));
             grabbingBall = distToBall <= 20 ? true : grabbingBall; 
@@ -268,7 +309,8 @@ public:
             break;
         case 'l':
             ROS_INFO("Leaving Manual Steering Mode");
-            ros::shutdown();
+            
+            shutdown();
             break;
         }
 
@@ -278,7 +320,7 @@ public:
             ballPos[1] = robotPos[1];
             msg.bola_x = ballPos[0];
             msg.bola_y = ballPos[1];
-            pc2bs_pub.publish(msg);
+            pubPC2BS.publish(msg);
         }
     }
 
@@ -298,17 +340,17 @@ public:
 
         // float th = 90 - degToRad(atan2(y, x));
 
-        // updateMotor(x, y, (th - bsMsg.th));
+        // updateMotor(x, y, (th - prevMsg.th));
 
         float x = target[0] - robotPos[0];
         float y = target[1] - robotPos[1];
 
-        if (x <= 0.1 && y <= 0.1) {
+        if (fabs(x) <= 0.1 && fabs(y) <= 0.1) {
             return;
         }
         if (currStep == 1) {
-            float th = 45 - degToRad(atan2(y, x));
-            float rotation = th - bsMsg.th;
+            float th = 50 - degToRad(atan2(y, x));
+            float rotation = th - prevMsg.th;
 
             if (fabs(rotation) < 5.0) {
                 currStep = 2;
@@ -336,71 +378,106 @@ public:
 
     void function_3()
     {
+        // float x = target[0] - robotPos[0];
+        // float y = target[1] - robotPos[1];
+
+        // if (x == 0 && y == 0)
+        //     return;
+
+        // float th = 90 - degToRad(atan2(y, x));
+
+        // updateMotor(x, y, (th - prevMsg.th));
+
+        if (blankFrame < 5) {
+            blankFrame++;
+            return;
+        }
+
         float x = target[0] - robotPos[0];
         float y = target[1] - robotPos[1];
 
-        if (x == 0 && y == 0)
+        if (fabs(x) <= 0.1 && fabs(y) <= 0.1) {
             return;
+        }
+        // float th = 50 - degToRad(atan2(y, x));
+        float th = angleBetweenVectors(target[0], target[1], robotPos[0], robotPos[1]);
+        float rotation = th - prevMsg.th;
+        ROS_INFO("TO %.2f %.2f", th, rotation);
 
-        float th = 90 - degToRad(atan2(y, x));
-
-        updateMotor(x, y, (th - bsMsg.th));
+        if (currStep == 1) {
+            if (fabs(rotation) < 10.0) {
+                currStep = 2;
+            } else {
+                updateMotor(0, 0, 200.0f);
+            }
+        } else if (currStep == 2) {
+            updateMotor(x, y, 0);
+        }
     }
 
-    void function_4(float &svDeg)
-    {
+    void function_4(float &offset) {
+    
         float x;
         float y;
         float th;
 
-        if (currStep == 1)
-        {
-            x = target[0] - robotPos[0];
-            y = target[1] - robotPos[1];
-            th = 90 - (degToRad(atan2(y, x)));
-            svDeg = bsMsg.th;
-            updateMotor(x, y, (th - (bsMsg.th)));
+        if (blankFrame < 10) {
+            blankFrame++;
+            return;
         }
-        else if (currStep == 2)
-        {
 
-            x = ballPos[0] - robotPos[0];
-            y = ballPos[1] - robotPos[1];
+        x = target[0] - robotPos[0];
+        y = target[1] - robotPos[1];
 
-            th = 0;
-            svDeg = 0;
-            updateMotor(x, y, th);
+        if (fabs(x) <= 0.1 && fabs(y) <= 0.1) {
+            return;
+        }
+        if (currStep == 1) {
+            float th = 50 - degToRad(atan2(y, x));
+            float rotation = th - prevMsg.th;
+
+            if (fabs(rotation) < 5.0) {
+                currStep = 2;
+            } else {
+                updateMotor(0, 0, rotation*5.0f);
+            }
+
+            ROS_INFO("ROTATING %.2f %.2f", th, rotation);
+        }
+        else if (currStep == 2) {
+            // UNFINISHED :))
+            updateMotor(x, y, 0);
         }
     }
 
     void run()
     {
-        float svDeg = 0;
+        float offset = 0;
 
-        ros::Rate rate(50);
-        while (ros::ok())
+        Rate rate(50);
+        while (ok())
         {
-            ros::spinOnce();
+            spinOnce();
 
-            if (bsMsg.status == 1)
+            if (prevMsg.status == 1)
             {
                 function_1();
                 updateMotor(0, 0, 0, true);
 
-            } else if (bsMsg.status == 2)
+            } else if (prevMsg.status == 2)
             {
                 msg.bola_x = ballPos[0];
                 msg.bola_y = ballPos[1];
-                pc2bs_pub.publish(msg);
+                pubPC2BS.publish(msg);
                 function_2();
 
-            } else if (bsMsg.status == 3)
+            } else if (prevMsg.status == 3)
             {
                 function_3();
 
-            } else if (bsMsg.status == 4)
+            } else if (prevMsg.status == 4)
             {
-                function_4(svDeg);
+                function_4(offset);
             }
 
             rate.sleep();
@@ -410,10 +487,10 @@ public:
 
 int main(int argc, char **argv)
 {
-    ros::init(argc, argv, "robot_controller");
+    init(argc, argv, "robot_controller");
 
-    Controller robot;
-    robot.run();
+    Controller controller;
+    controller.run();
 
     return 0;
 }
